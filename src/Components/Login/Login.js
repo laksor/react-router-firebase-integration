376b81bb8c730ca983c7505724bb9a1f82c3f486
laskor <al.moksud@gmail.com>
2022-04-14 04:39:37 -0400

    style added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import './Login.css';
-
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import "./Login.css";
 
 const Login = () => {
-    return (
-        <div className='text-center'>
-            <h1>log</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="fw-bold mb-5 text-center">log in</h1>
+      <Form className="w-50 mx-auto">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" className="rounded-pill" type="submit">
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Login;
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -5,8 +5,13 @@ import "./Register.css";
 const Register = () => {
   return (
     <div>
-      <h1 className="text-center">Register</h1>
+      <h1 className="text-center fw-bold mb-5">Register</h1>
       <Form className="mx-auto w-50">
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter name" />
+        </Form.Group>
+
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
@@ -19,9 +24,9 @@ const Register = () => {
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        
-        <Button variant="primary" type="submit">
-          Submit
+
+        <Button variant="primary" className="rounded-pill" type="submit">
+          Register
         </Button>
       </Form>
     </div>
